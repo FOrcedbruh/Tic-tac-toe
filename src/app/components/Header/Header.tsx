76ec33e7ebfe47696be571ff6641c5f536eba4c3
Илья Чапля <_ilchpl_@MacBook-Air-Ila.local>
2024-11-07 11:12:04 +0300

    add tg button

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -2,6 +2,7 @@ import { Dispatch, FC, SetStateAction } from "react";
 import styles from './Header.module.scss'
 import { motion } from "framer-motion";
 import { TPlayer } from "../../App";
+import TelegramLoginButton from "../TelegramLoginButton/TelegramLoginButton";
 
 
 interface IHeaderProps {
@@ -19,8 +20,20 @@ export const Header: FC<IHeaderProps> = ({ values, setValues, setPlayer }) => {
         setPlayer("cross")
     }
 
+    const handleBot = (user: any) => {
+        console.log(user)
+    }
+
     return (
         <header className={styles.header}>
+            <TelegramLoginButton
+                botName="TicTacToeJS_bot"
+                buttonSize="medium"
+                cornerRadius={3}
+                usePic={false}
+                dataOnauth={handleBot}
+                className={styles.tgBtn}
+            />
             <motion.button onClick={resetHandler} whileTap={{ y: 5}} className={styles.reset}>
                 Reset
             </motion.button>
